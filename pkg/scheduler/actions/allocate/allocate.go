a73abb209a1198241c8a96a2220fd60a25c3ea38
Zhang Jinghui <zhangjinghui5@huawei.com>
2019-12-23 19:09:50 +0800

    add node to back

--- pkg/scheduler/actions/allocate/allocate.go
+++ pkg/scheduler/actions/allocate/allocate.go
@@ -231,6 +231,13 @@ func (alloc *allocateAction) Execute(ssn *framework.Session) {
 						klog.Errorf("Failed to pipeline Task %v on %v in Session %v for %v.",
 							task.UID, node.Name, ssn.UID, err)
 					}
+					for i, n := range allNodes {
+						if n.Name == node.Name {
+							allNodes = append(allNodes[:i], allNodes[i+1:]...)
+							break
+						}
+					}
+					allNodes = append(allNodes, node)
 				}
 			}
 
